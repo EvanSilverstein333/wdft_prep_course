6cfefa0dcd155d185762f953a71f3e631a298114
EvanSilverstein <evan333es@gmail.com>
2017-09-15 22:55:18 -0400

    completed assignment 1

--- assignment_1/assignment1.js
+++ assignment_1/assignment1.js
@@ -5,6 +5,8 @@ Initialize an empty array below in the variable numbers.
 var numbers;
 console.log('hello world')
 
+numbers = new Array();
+
 /*
 Exercise #2
 Using the loop provided for you below, use an array's push method to insert the numbers 0 to 10 into positions 0 through 9 of the numbers array you just initialized above.'If you're not sure how to use the array push method go back and re read this module on data structures.'
@@ -12,15 +14,19 @@ Using the loop provided for you below, use an array's push method to insert the 
 
 for(var i=0;i<10;i++){
     //use the array push() method here.
+    numbers.push(i);
 }
 
 
 /*
 Exercise #3
-Test that you used the array push method correctly by printing the sum of the values at position 3 and 6 of the array (use the console.log() function to print to the console). 
+Test that you used the array push method correctly 
+by printing the sum of the values at position 3 and 6 of 
+the array (use the console.log() function to print to the console). 
 The outputted value should be 9.
 */
-
+var x = numbers[3] + numbers[6];
+console.log(x);
 
 
 
@@ -30,11 +36,11 @@ Use the console.log() function and the array .pop() to add all the values in the
 */
 
 var sum = 0;
-/* remove this line to uncomment the code block
 while(numbers.length != 0){
     //write pop function here
+    sum = sum + numbers.pop();
+
 }
-remove this line to uncomment the code block */
 console.log(sum);
 
 
@@ -42,12 +48,14 @@ console.log(sum);
 Exercise #5
 Create an empty object and assign it to the variable car below.
 */
-var car;
+var obj = {};
+var car = obj;
 
 /*
 Exercise #5
 Give the car object a property called "colour" and assign it the value of "black". Test that your code works correctly by outputting the "colour" property to the console. You should see the value "black" printed out.
 */
-
+car.colour = "black";
+console.log(car.colour);
 
 
